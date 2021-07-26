ecd79fae7a8b343c7d52781a5d06cd31f2336e2b
linji <237525603@qq.com>
2021-07-26 18:00:22 +0800

    add animate

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -9,108 +9,123 @@ import * as Three from "three";
 import Stats from "three/examples/jsm/libs/stats.module";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 
+const obj = {
+    theta: 0,
+    stats: null,
+    camera: null,
+    scene: null,
+    renderer: null,
+    mixer: null,
+    radius: 600,
+    prevTime: Date.now(),
+    container: null,
+    modelPath: 'models/Horse.glb'
+};
+
 export default {
   name: "Index",
-  data() {
-    return {
-      theta: 0,
-      stats: null,
-      camera: null,
-      scene: null,
-      renderer: null,
-      mesh: null,
-      mixer: null,
-      radius: 600,
-      prevTime: Date.now(),
-      container: null,
-      modelPath: 'models/Horse.glb'
-    };
-  },
   methods: {
     init() {
-      this.container = document.getElementById("container");
+      obj.container = document.getElementById("container");
 
-      this.camera = new Three.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-      this.camera.position.y = 300;
+      obj.camera = new Three.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+      obj.camera.position.y = 100;
 
-      this.scene = new Three.Scene();
-      this.scene.background = new Three.Color(0xf0f0f0);
+      obj.scene = new Three.Scene();
+      obj.scene.background = new Three.Color(0xf0f0f0);
 
       const light1 = new Three.DirectionalLight(0xefefff, 1.5);
       light1.position.set(1, 1, 1).normalize();
-      this.scene.add(light1);
+      obj.scene.add(light1);
 
       const light2 = new Three.DirectionalLight(0xffefef, 1.5);
       light2.position.set(-1, -1, -1).normalize();
-      this.scene.add(light2);
+      obj.scene.add(light2);
 
       const loader = new GLTFLoader();
 
-      loader.load( this.modelPath,  ( gltf ) => {
-        this.mesh = gltf.scene.children[ 0 ];
-        this.mesh.scale.set( 1.5, 1.5, 1.5 );
-        this.scene.add( this.mesh );
+      loader.load( obj.modelPath,  ( gltf ) => {
+        let mesh = gltf.scene.children[ 0 ];
+         mesh.scale.set( 0.7, 0.7, 0.7 );
 
-        this.mixer = new Three.AnimationMixer( this.mesh );
+         mesh.position.x = -101
+         mesh.position.y = 100
 
-        this.mixer.clipAction( gltf.animations[ 0 ] ).setDuration( 1 ).play();
+        obj.scene.add( mesh );
+
+        obj.mixer = new Three.AnimationMixer( mesh );
+
+        obj.mixer.clipAction( gltf.animations[ 0 ] ).setDuration( 1 ).play();
       } );
 
-      this.renderer = new Three.WebGLRenderer();
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-      this.renderer.outputEncoding = Three.sRGBEncoding;
+    loader.load( obj.modelPath,  ( gltf ) => {
+        let mesh = gltf.scene.children[ 0 ];
+        mesh.scale.set( 0.5, 0.5, 0.5 );
 
-      this.container.appendChild(this.renderer.domElement);
+        obj.scene.add( mesh );
 
-      this.stats = new Stats();
-      this.container.appendChild(this.stats.dom);
+        let mixer = new Three.AnimationMixer( mesh );
+
+         mixer.clipAction( gltf.animations[ 0 ] ).setDuration( 1 ).play();
+    } );
+
+
+      obj.renderer = new Three.WebGLRenderer();
+      obj.renderer.setPixelRatio(window.devicePixelRatio);
+      obj.renderer.setSize(window.innerWidth, window.innerHeight);
+
+      obj.renderer.outputEncoding = Three.sRGBEncoding;
+
+      obj.container.appendChild(obj.renderer.domElement);
+
+      obj.stats = new Stats();
+      obj.container.appendChild(obj.stats.dom);
 
       window.addEventListener('resize', this.onWindowResize);
     },
     render() {
-      this.theta += 0.1;
+      obj.theta += 0.9;
 
-      this.camera.position.x = this.radius * Math.sin(Three.MathUtils.degToRad(this.theta));
-      this.camera.position.z = this.radius * Math.cos(Three.MathUtils.degToRad(this.theta));
+      obj.camera.position.x = obj.radius * Math.sin(Three.MathUtils.degToRad(50));
+      obj.camera.position.z = obj.radius * Math.cos(Three.MathUtils.degToRad(90));
 
-      this.camera.lookAt(0, 150, 0);
+      obj.camera.lookAt(0, 150, 0);
 
-      if (this.mixer) {
-
+      if (obj.mixer) {
         const time = Date.now();
 
-        this.mixer.update((time - this.prevTime) * 0.001);
+        obj.mixer.update((time - obj.prevTime) * 0.001);
 
-        this.prevTime = time;
-
+        obj.prevTime = time;
       }
 
-      this.renderer.render(this.scene, this.camera);
+      obj.renderer.render(obj.scene, obj.camera);
     },
     onWindowResize() {
-      this.camera.aspect = window.innerWidth / window.innerHeight;
-      this.camera.updateProjectionMatrix();
+      obj.camera.aspect = window.innerWidth / window.innerHeight;
+      obj.camera.updateProjectionMatrix();
 
-      renderer.setSize(window.innerWidth, window.innerHeight);
+      obj.renderer.setSize(window.innerWidth, window.innerHeight);
     },
     animate() {
       requestAnimationFrame(this.animate);
 
       this.render();
 
-      this.stats.update();
+      obj.stats.update();
     }
 
   },
-  created() {
-    this.init();
-    this.animate();
-  },
+  mounted () {
+      this.init();
+      this.animate();
+  }
 };
 </script>
 <style>
 #container {
 }
+body { margin: 0; }
+canvas { width: 100%; height: 100% }
 </style>
