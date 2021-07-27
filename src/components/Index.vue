b1d6caa6240748baaaebb6cd6037c52e2a1d79ef
linji <237525603@qq.com>
2021-07-27 19:07:42 +0800

    add animate

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,131 +1,135 @@
 <template>
-  <div>
-    <div id="container"></div>
-  </div>
+    <div>
+        <button id="add" @click="add" style="float: left;position: absolute;">增加</button>
+
+        <div id="container"></div>
+    </div>
 </template>
 
 <script>
 import * as Three from "three";
-import Stats from "three/examples/jsm/libs/stats.module";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 
-const obj = {
-    theta: 0,
-    stats: null,
-    camera: null,
-    scene: null,
-    renderer: null,
-    mixer: null,
-    radius: 600,
-    prevTime: Date.now(),
-    container: null,
-    modelPath: 'models/Horse.glb'
-};
+let scene,container,camera,renderer,loader = null;
+let meshes = [];
+let theta = 0;
+let radius = 900;
+let modelPath = 'models/Horse.glb'
+
+let pos = [0,-200,200,-400,400]
+
+class Horse {
+    mixer; // 模型动画
+    prevTime; // 上一次更新时间
+    size; // 模型尺寸
+    obj; // mesh 对象
+    speed;
+
+    constructor(obj,size=2,speed=0.0013) {
+        this.mixer = null
+        this.prevTime = Date.now()
+        this.size = size
+        this.obj = obj
+        this.speed = speed
+    }
+}
 
 export default {
-  name: "Index",
-  methods: {
-    init() {
-      obj.container = document.getElementById("container");
+    name: "Index",
+    methods: {
+        add() {
+            loader.load(modelPath, (gltf) => {
+                let mesh = new Horse(gltf.scene.children[0]);
+                mesh.obj.scale.set(1.8, 1.8, 1.8);
 
-      obj.camera = new Three.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-      obj.camera.position.y = 100;
+                mesh.obj.position.x = pos.shift();
+                mesh.obj.position.y = -20;
+                mesh.obj.position.z = -100;
 
-      obj.scene = new Three.Scene();
-      obj.scene.background = new Three.Color(0xf0f0f0);
+                scene.add(mesh.obj);
 
-      const light1 = new Three.DirectionalLight(0xefefff, 1.5);
-      light1.position.set(1, 1, 1).normalize();
-      obj.scene.add(light1);
+                mesh.mixer = new Three.AnimationMixer(mesh.obj);
+                mesh.mixer.clipAction(gltf.animations[0]).setDuration(1).play();
 
-      const light2 = new Three.DirectionalLight(0xffefef, 1.5);
-      light2.position.set(-1, -1, -1).normalize();
-      obj.scene.add(light2);
-
-      const loader = new GLTFLoader();
-
-      loader.load( obj.modelPath,  ( gltf ) => {
-        let mesh = gltf.scene.children[ 0 ];
-         mesh.scale.set( 0.7, 0.7, 0.7 );
-
-         mesh.position.x = -101
-         mesh.position.y = 100
-
-        obj.scene.add( mesh );
-
-        obj.mixer = new Three.AnimationMixer( mesh );
-
-        obj.mixer.clipAction( gltf.animations[ 0 ] ).setDuration( 1 ).play();
-      } );
+                // setTimeout(()=>{mesh.mixer.clipAction(gltf.animations[0]).setDuration(1).stop();},2000)
 
 
-    loader.load( obj.modelPath,  ( gltf ) => {
-        let mesh = gltf.scene.children[ 0 ];
-        mesh.scale.set( 0.5, 0.5, 0.5 );
+                meshes.push(mesh)
+            });
+        },
+        init() {
+            container = document.getElementById("container");
 
-        obj.scene.add( mesh );
+            camera = new Three.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+            camera.position.y = 100;
 
-        let mixer = new Three.AnimationMixer( mesh );
+            scene = new Three.Scene();
+            scene.background = new Three.Color(0xf0f0f0);
 
-         mixer.clipAction( gltf.animations[ 0 ] ).setDuration( 1 ).play();
-    } );
+            const light1 = new Three.DirectionalLight(0xefefff, 1.5);
+            light1.position.set(1, 1, 1).normalize();
+            scene.add(light1);
 
+            const light2 = new Three.DirectionalLight(0xffefef, 1.5);
+            light2.position.set(-1, -1, -1).normalize();
+            scene.add(light2);
 
-      obj.renderer = new Three.WebGLRenderer();
-      obj.renderer.setPixelRatio(window.devicePixelRatio);
-      obj.renderer.setSize(window.innerWidth, window.innerHeight);
+            loader = new GLTFLoader();
 
-      obj.renderer.outputEncoding = Three.sRGBEncoding;
+            renderer = new Three.WebGLRenderer();
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setSize(window.innerWidth, window.innerHeight);
 
-      obj.container.appendChild(obj.renderer.domElement);
+            renderer.outputEncoding = Three.sRGBEncoding;
 
-      obj.stats = new Stats();
-      obj.container.appendChild(obj.stats.dom);
+            container.appendChild(renderer.domElement);
 
-      window.addEventListener('resize', this.onWindowResize);
+            window.addEventListener('resize', this.onWindowResize);
+        },
+        onWindowResize() {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        },
+        render() {
+            theta += 0.9;
+
+            // camera.position.x = radius * Math.sin(Three.MathUtils.degToRad(theta));
+            // camera.position.y = radius * Math.cos(Three.MathUtils.degToRad(380));
+            camera.position.z = radius * Math.cos(Three.MathUtils.degToRad(20));
+
+            camera.lookAt(0, 150, 0);
+
+            for (let mesh of meshes) {
+                const time = Date.now();
+                mesh.mixer.update((time - mesh.prevTime) * mesh.speed);
+                mesh.prevTime = time;
+            }
+            renderer.render(scene, camera);
+        },
+        animate() {
+            requestAnimationFrame(this.animate);
+            this.render();
+        }
     },
-    render() {
-      obj.theta += 0.9;
-
-      obj.camera.position.x = obj.radius * Math.sin(Three.MathUtils.degToRad(50));
-      obj.camera.position.z = obj.radius * Math.cos(Three.MathUtils.degToRad(90));
-
-      obj.camera.lookAt(0, 150, 0);
-
-      if (obj.mixer) {
-        const time = Date.now();
-
-        obj.mixer.update((time - obj.prevTime) * 0.001);
-
-        obj.prevTime = time;
-      }
-
-      obj.renderer.render(obj.scene, obj.camera);
-    },
-    onWindowResize() {
-      obj.camera.aspect = window.innerWidth / window.innerHeight;
-      obj.camera.updateProjectionMatrix();
-
-      obj.renderer.setSize(window.innerWidth, window.innerHeight);
-    },
-    animate() {
-      requestAnimationFrame(this.animate);
-
-      this.render();
-
-      obj.stats.update();
+    mounted() {
+        this.init();
+        this.animate();
+        this.add();
     }
-
-  },
-  mounted () {
-      this.init();
-      this.animate();
-  }
 };
 </script>
 <style>
 #container {
 }
-body { margin: 0; }
-canvas { width: 100%; height: 100% }
+
+body {
+    margin: 0;
+}
+
+canvas {
+    width: 100%;
+    height: 100%
+}
 </style>
